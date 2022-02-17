2e263cadf4438987fac33a86a3587e3f92559636
Furkan coşgun <furkan51cosgun@gmail.com>
2022-02-17 15:49:46 +0300

    c# paint uygulaması

---
+++ paintapp/paintapp/paintodev.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace paintapp
+{
+    partial class paintodev
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(paintodev));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Yeniac = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.cmbkalempx = new System.Windows.Forms.ToolStripComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(932, 527);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove_1);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.Yeniac,
+            this.toolStripSeparator2,
+            this.toolStripButton2,
+            this.toolStripSeparator3,
+            this.toolStripButton3,
+            this.toolStripSeparator4,
+            this.toolStripLabel1,
+            this.cmbkalempx});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(932, 28);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(79, 25);
+            this.toolStripButton1.Text = "Kaydet";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 28);
+            // 
+            // Yeniac
+            // 
+            this.Yeniac.Image = ((System.Drawing.Image)(resources.GetObject("Yeniac.Image")));
+            this.Yeniac.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Yeniac.Name = "Yeniac";
+            this.Yeniac.Size = new System.Drawing.Size(100, 25);
+            this.Yeniac.Text = "Yeni Sayfa";
+            this.Yeniac.Click += new System.EventHandler(this.Yeniac_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 28);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(65, 25);
+            this.toolStripButton2.Text = "Renk";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 28);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(78, 25);
+            this.toolStripButton3.Text = "Geri Al";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 28);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(60, 25);
+            this.toolStripLabel1.Text = "Kalınlık:";
+            // 
+            // cmbkalempx
+            // 
+            this.cmbkalempx.BackColor = System.Drawing.Color.Silver;
+            this.cmbkalempx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbkalempx.Name = "cmbkalempx";
+            this.cmbkalempx.Size = new System.Drawing.Size(121, 28);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(150, 150);
+            this.toolStripContainer1.Location = new System.Drawing.Point(8, 8);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(150, 175);
+            this.toolStripContainer1.TabIndex = 3;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // paintodev
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 527);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "paintodev";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "kaydet";
+            this.Load += new System.EventHandler(this.kaydet_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripComboBox cmbkalempx;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripButton Yeniac;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+    }
+}
---
+++ paintapp/paintapp/paintodev.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paintapp
+{
+    public partial class paintodev : Form
+    {
+        public paintodev()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(panel1.ClientSize.Width, panel1.ClientSize.Height);
+        }
+        Graphics g;
+        bool ciz = false;
+        int baslaX, baslaY;
+        Bitmap bmp;
+        Stack<Bitmap> gerial = new Stack<Bitmap>();
+        private void kaydet_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                cmbkalempx.Items.Add(i + "px");
+            }
+        }
+        private void panel1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (ciz)
+            {
+                using (g = Graphics.FromImage(bmp))
+                {
+                    Pen p = new Pen(colorDialog1.Color, float.Parse(cmbkalempx.SelectedIndex.ToString()));
+                    g.DrawLine(p, new Point(baslaX, baslaY), new Point(e.X, e.Y));
+                    baslaX = e.X;
+                    baslaY = e.Y;
+                }
+                panel1.Refresh();
+            }
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ciz = true;
+            baslaX = e.X;
+            baslaY = e.Y;
+            gerial.Push((Bitmap)bmp.Clone());
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ciz = false;
+        }
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, Point.Empty);
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfg = new SaveFileDialog();
+            sfg.Title = "Resim dosyaları";
+            sfg.DefaultExt = ".png";
+            sfg.Filter = "Resim Dosyaları (*.png)|*.png|Tüm Dosyalar(*.*)|*.*";
+            if (sfg.ShowDialog() == DialogResult.OK)
+            {
+                bmp.Save(sfg.FileName, ImageFormat.Png);
+            }
+        }
+        private void Yeniac_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Çalışmanız silinecek, devam etmek istediğinize emin misiniz", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(Color.White);
+                    panel1.Refresh();
+                }
+
+            }
+        }
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (gerial.Count > 0)
+            {
+                bmp = gerial.Pop();
+                g = Graphics.FromImage(bmp);
+                panel1.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Geri alınacak çizim bulunamadı !");
+            }
+        }
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+    }
+}
